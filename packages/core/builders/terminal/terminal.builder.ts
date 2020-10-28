af79dd30efc6cfe67bafe1aba18c3680ceb660e6
Romullo <developermarsh@gmail.com>
2020-10-28 20:42:34 -0300

    chore(core):  terminal builder

--- packages/core/builders/terminal/terminal.builder.ts
+++ packages/core/builders/terminal/terminal.builder.ts
@@ -1,20 +1,72 @@
 import { BaseBuilder } from '../base'
-import { ITerminalBuilder } from './terminal.interface'
+import { ITerminalBuilder, IProps, TExtension } from './terminal.interface'
 
 export class TerminalBuilder extends BaseBuilder implements ITerminalBuilder {
+  private extension: TExtension
+
+  constructor({ rootDir, extension }: IProps) {
+    super({ rootDir })
+    this.extension = extension
+  }
+
+  /**
+   * @method createVariants
+   *
+   * Create new theme variants.
+   */
   createVariants(): string {
-    throw new Error('Method not implemented.')
+    this.theme.final[this.theme.stage.variant] = this.theme.stage.payload
+    return 'created'
   }
 
+  /**
+   * @function merge
+   *
+   * Where the magic happens.
+   *
+   * @param theme
+   */
   merge(theme: any): void {
-    throw new Error('Method not implemented.')
+    this.theme.stage = theme
+    this.setColors().createVariants()
   }
 
+  /**
+   * @method stage
+   *
+   * Stage method where the finishing touches are applied.
+   */
   stage(): void {
-    throw new Error('Method not implemented.')
+    this.listThemes.forEach(themeName => {
+      const [variant] = themeName.split('.')
+
+      this.merge({
+        payload: this.getFile(
+          `${this.rootDir.themes}/common/base.${this.extension}`,
+        ),
+        variant,
+      })
+    })
   }
 
+  /**
+   * @method compile
+   *
+   * Responsible for the construction.
+   */
   compile(): string {
-    throw new Error('Method not implemented.')
+    this.stage()
+
+    if (this.theme.final) {
+      Object.entries(this.theme.final).map(([name, theme]) =>
+        this.createFile({
+          path: this.rootDir.build,
+          file: theme,
+          fileName: `${name}.${this.extension}`,
+        }),
+      )
+    }
+
+    return 'done'
   }
 }
--- packages/core/builders/terminal/terminal.interface.ts
+++ packages/core/builders/terminal/terminal.interface.ts
@@ -1,3 +1,8 @@
-import { IBuilderCommon } from '../base'
+import { IBuilderCommon, TBaseProps } from '../base'
 
 export interface ITerminalBuilder extends IBuilderCommon {}
+
+export type TExtension = 'conf' | 'yml'
+export interface IProps extends TBaseProps {
+  extension: TExtension
+}
