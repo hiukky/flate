f55ec2b53d953c52fa745f93f2cae38d5d6b173d
Romullo <developermarsh@gmail.com>
2020-11-21 13:42:11 -0300

    fix(themes): fixes in the installation script for alacritty

--- packages/themes/alacritty/scripts/install.sh
+++ packages/themes/alacritty/scripts/install.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 
-path_theme="$HOME/.config/alacritty"
+declare FLAG=$(echo "$@")
+declare PATH_THEME="$HOME/.config/alacritty/alacritty.yml"
+declare VARIANTS=('flate' 'flate-arc' 'flate-punk')
 
-python -c "import yq" &> /dev/null
+yq &> /dev/null
 
-if [[ "$?" -eq 1 ]]; then
-  pip install yq -U
+if [[ "$?" -eq 127 ]]; then
+  sudo snap install yq
 fi
 
-read() {
-  theme="$(yq .colors $1 --yaml-output)"
-  echo "${theme}"
+install() {
+ [[ -z $1 ]] && variant='flate' || variant=$1
+
+ if ! printf '%s\n' "${VARIANTS[@]}" | grep -P "^$variant$" > /dev/null; then
+  echo 'Invalid theme name!'
+  exit 0
+ fi
+
+ yq d -i $PATH_THEME 'colors'
+ yq m -i -I 4 $PATH_THEME $PWD/dist/$variant.yml
+
+ echo "Instaled!!"
 }
 
-echo $(yq w "$path_theme/alacritty.yml" 'colors' "$(read $PWD/dist/flate.yml)")
+echo
+install $FLAG
