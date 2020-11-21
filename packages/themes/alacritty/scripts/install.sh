30515f8c3b3869667561e825e885390990a3c6ad
Romullo <developermarsh@gmail.com>
2020-11-21 15:34:55 -0300

    chore(themes): alacritty  online installation

--- packages/themes/alacritty/scripts/install.sh
+++ packages/themes/alacritty/scripts/install.sh
@@ -2,13 +2,21 @@
 
 declare FLAG=$(echo "$@")
 declare PATH_THEME="$HOME/.config/alacritty/alacritty.yml"
+declare URL_THEME="https://raw.githubusercontent.com/hiukky/flate/develop/packages/themes/alacritty/release"
 declare VARIANTS=('flate' 'flate-arc' 'flate-punk')
+declare WORKDIR="$HOME/.tmp/flate"
 
 yq &> /dev/null
 
 if [[ "$?" -eq 127 ]]; then
   sudo snap install yq
 fi
+
+get() {
+ mkdir -p $WORKDIR
+ curl GET -s $URL_THEME/$1.yml > $WORKDIR/$1.yml
+ sleep 3
+}
 
 install() {
  [[ -z $1 ]] && variant='flate' || variant=$1
@@ -18,8 +26,11 @@ install() {
   exit 0
  fi
 
+ get $variant
+
  yq d -i $PATH_THEME 'colors'
- yq m -i -I 4 $PATH_THEME $PWD/dist/$variant.yml
+ yq m -i -I 4 $PATH_THEME $WORKDIR/$variant.yml
+ rm -rf $WORKDIR
 
  echo "Instaled!!"
 }
